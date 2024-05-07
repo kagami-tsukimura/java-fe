f0afb58325316f4c2e00edf13d706e1d396a561c
Kagami <shimura.kyohei7617@gmail.com>
2024-05-07 20:02:11 +0900

    auto: auto command push

--- src/sec3/Main.java
+++ src/sec3/Main.java
@@ -2,6 +2,7 @@ package sec3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World3!");
+        int age = 20;
+        System.out.println(age);
     }
 }
