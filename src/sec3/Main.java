8f0ab4e0614cf429f792e052bb196e5408b56120
Kagami <shimura.kyohei7617@gmail.com>
2024-05-07 21:06:18 +0900

    auto: auto command push

--- src/sec3/Main.java
+++ src/sec3/Main.java
@@ -4,26 +4,22 @@ public class Main {
     public static void main(String[] args) {
         // int: 整数型
         int age = 20;
-        System.out.println(age);
-
         // double: 浮動小数点型
         double pi = 3.14;
-        System.out.println(pi);
-
-        System.out.println(age + pi);
-
         // String: 文字列型
         String str1 = "Hello";
-        System.out.println(str1 + age + pi);
-        System.out.println(str1 + (age + pi));
-
         // char: 文字型
         char char1  = 'A';
-        System.out.println(char1);
-
         // final: 定数
         final int a = 0;
         // a = 1;
+
+        System.out.println(age);
+        System.out.println(pi);
+        System.out.println(age + pi);
+        System.out.println(str1 + age + pi);
+        System.out.println(str1 + (age + pi));
+        System.out.println(char1);
         System.out.println(a);
     }
 }
