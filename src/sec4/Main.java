fb9929af6265e9f617898aa6e6809e7167276cde
Kagami <shimura.kyohei7617@gmail.com>
2024-05-07 21:34:36 +0900

    auto: auto command push

--- src/sec4/Main.java
+++ src/sec4/Main.java
@@ -2,7 +2,7 @@ package sec4;
 public class Main {
     public static void main(String[] args) {
         int ret;
-        int age = 56;
+        int age = 9;
 
         if (age <= 3) {
             ret = 100;
