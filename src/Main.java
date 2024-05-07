0e9c284a98cac5b6d44ebcc9bd6709de307e9108
Kagami <shimura.kyohei7617@gmail.com>
2024-05-07 19:37:52 +0900

    auto: auto command push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // commentout this line
         System.out.println("Hello World!");
         System.out.println("Hello FE!");
         System.out.println("Hello Java!");
