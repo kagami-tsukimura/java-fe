d9507f87fbf9a6418289e5f7421c476b5c21f43e
Kagami <shimura.kyohei7617@gmail.com>
2024-05-07 20:54:13 +0900

    auto: auto command push

--- src/sec3/Main.java
+++ src/sec3/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         int age = 20;
         System.out.println(age);
+
+        double pi = 3.14;
+        System.out.println(pi);
     }
 }
