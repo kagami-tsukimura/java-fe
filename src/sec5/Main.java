6133935cbf459f8b6a9aa389310a4ca6a77a5515
Kagami <shimura.kyohei7617@gmail.com>
2024-05-08 21:30:04 +0900

    auto: auto command push

--- src/sec5/Main.java
+++ src/sec5/Main.java
@@ -3,8 +3,12 @@ package sec5;
 public class Main {
     public static void main(String[] args) {
         int value;
+        int[] array;
+
         value = 100;
+        array = new int[] {1, 2, 3, value};
 
         System.out.println("value = " + value);
+        System.out.println(array[3]);
     }
 }
