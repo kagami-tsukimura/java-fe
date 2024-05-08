cdc59a542bb9a314df72a719f8406cec610bfc2b
Kagami <shimura.kyohei7617@gmail.com>
2024-05-08 21:31:49 +0900

    auto: auto command push

--- src/sec5/Main.java
+++ src/sec5/Main.java
@@ -4,11 +4,17 @@ public class Main {
     public static void main(String[] args) {
         int value;
         int[] array;
+        int[] result = new int[3];
 
         value = 100;
+        result[0] = 200;
+        result[1] = 200;
+        result[2] = 200;
+
         array = new int[] {1, 2, 3, value};
 
         System.out.println("value = " + value);
         System.out.println(array[3]);
+        System.out.println(result[2]);
     }
 }
