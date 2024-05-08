91057e9ae16e1359cef379689d5a03e7abc591d3
Kagami <shimura.kyohei7617@gmail.com>
2024-05-08 21:41:30 +0900

    auto: auto command push

--- src/sec5/Main.java
+++ src/sec5/Main.java
@@ -3,7 +3,7 @@ package sec5;
 public class Main {
     public static void main(String[] args) {
         int value;
-        int[] array;
+        int[] array1;
         int[] result = new int[3];
 
         value = 1;
@@ -11,11 +11,17 @@ public class Main {
         result[1] = 200;
         result[2] = 300;
 
-        array = new int[] {1, 2, 3, value};
+        array1 = new int[] {1, 2, 3, value};
 
         System.out.println("value = " + value);
-        System.out.println(array[3]);
+        System.out.println(array1[3]);
         System.out.println(result[2]);
         System.out.println(result[value]);
+
+        int[] array = new int[] {1, 2, 3, 4, 5};
+        int right, left;
+        int tmp;
+
+        System.out.println("array = " + array[0] + ", " + array[1] + ", " + array[2] + ", " + array[3] + ", " + array[4]);
     }
 }
