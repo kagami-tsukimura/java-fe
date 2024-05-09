7f480904cdfcc16b41afc891bef8564916c285e5
Kagami <shimura.kyohei7617@gmail.com>
2024-05-09 21:18:02 +0900

    auto: auto command push

--- src/sec6/Main.java
+++ src/sec6/Main.java
@@ -33,5 +33,12 @@ public class Main {
             System.out.println(n);
             n++;
         }while(n <= 10);
+
+        int ary[] = new int[] {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < ary.length; i++) {
+            System.out.println(ary[i]);
+        }
+
     }
 }
