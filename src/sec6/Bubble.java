577a31573ce4abc6a84960ecaebe98b34980d51f
Kagami <shimura.kyohei7617@gmail.com>
2024-05-09 21:23:03 +0900

    auto: auto command push

--- src/sec6/Bubble.java
+++ src/sec6/Bubble.java
@@ -2,6 +2,18 @@ package sec6;
 
 public class Bubble {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] A = new int[] {10, 2, 23, 4, 15};
+
+        System.out.println("A = " + A[0] + ", " + A[1] + ", " + A[2] + ", " + A[3] + ", " + A[4]);
+        for(int i = 1; i <= A.length - 1; i++) {
+            for(int j = 0; j <= A.length - i - 1; j++) {
+                if(A[j] > A[j + 1]) {
+                    int tmp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = tmp;
+                }
+            }
+        }
+        System.out.println("A = " + A[0] + ", " + A[1] + ", " + A[2] + ", " + A[3] + ", " + A[4]);
     }
 }
