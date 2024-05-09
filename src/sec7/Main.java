50234ee8d40d0a4932becbb2f774e47ace482058
Kagami <shimura.kyohei7617@gmail.com>
2024-05-09 22:42:32 +0900

    auto: auto command push

--- src/sec7/Main.java
+++ src/sec7/Main.java
@@ -10,10 +10,21 @@ public class Main {
         int num2 = rand.nextInt(10) + 100;
         function("Hello", rand.nextInt(10) + 100);
         function(msg2, num2);
+
+        String msg = function2("Hello");
+        System.out.println(msg + "World");
     }
 
     public static void function(String msg, int number) {
         System.out.println(msg);
         System.out.println(number);
+    }
+
+    public static String function2(String msg) {
+        System.out.println("------");
+        msg = "[" + msg + "]";
+        System.out.println(msg);
+        System.out.println("------");
+        return msg;
     }
 }
