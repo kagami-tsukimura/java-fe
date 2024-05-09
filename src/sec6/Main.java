ac37730962e748fafe8dd98fd22dd46759b4ac9e
Kagami <shimura.kyohei7617@gmail.com>
2024-05-09 21:17:06 +0900

    auto: auto command push

--- src/sec6/Main.java
+++ src/sec6/Main.java
@@ -19,7 +19,7 @@ public class Main {
             array[left] = tmp;
         }
 
-        System.out.println("");
+        System.out.println("-----");
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
