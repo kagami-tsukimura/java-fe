5ff4c34037aea0dd5deb5d810a297dfdfa79a993
Kagami <shimura.kyohei7617@gmail.com>
2024-05-09 22:36:23 +0900

    auto: auto command push

---
+++ src/sec7/Bubble.java
@@ -0,0 +1,29 @@
+package sec7;
+
+public class Bubble {
+    public static void main(String[] args) {
+        int[] A = new int[] {100, 0, 15, 22, 69, 985, 47};
+        
+        A = bubbleSort(A);
+        arrayPrint(A);
+    }
+
+    public static int[] bubbleSort(int[] A) {
+        for(int i = 0; i < A.length ; i++) {
+            for(int j = A.length -1; j >= i+1 ; j--){
+                if(A[j] < A[j-1]){
+                    int w = A[j];
+                    A[j] = A[j-1];
+                    A[j-1] = w;
+                }
+            }
+        }
+        return A;
+    }
+
+    public static void arrayPrint(int[] A) {
+        for(int i = 0; i < A.length ; i++) {
+            System.out.println(A[i]);
+        }
+    }
+}
