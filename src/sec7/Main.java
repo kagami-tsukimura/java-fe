8d3d802e93e1ead10a230d10640c422a0831b180
Kagami <shimura.kyohei7617@gmail.com>
2024-05-09 22:29:49 +0900

    auto: auto command push

--- src/sec7/Main.java
+++ src/sec7/Main.java
@@ -5,13 +5,17 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String msg = "Hello";
+        String msg2 = "World";
         Random rand = new Random();
         int num = rand.nextInt(10) + 100;
-        function(num);
+        int num2 = rand.nextInt(10) + 100;
+        function(msg, num);
+        function(msg2, num2);
     }
 
-    public static void function(int number) {
-        System.out.println("function");
+    public static void function(String msg, int number) {
+        System.out.println(msg);
         System.out.println(number);
     }
     
