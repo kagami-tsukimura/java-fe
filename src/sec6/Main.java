99e314efc67823738cb06bad4e2806cce0efccff
Kagami <shimura.kyohei7617@gmail.com>
2024-05-09 20:48:12 +0900

    auto: auto command push

--- src/sec6/Main.java
+++ src/sec6/Main.java
@@ -7,5 +7,17 @@ public class Main {
         for (int num = 10; num > 0; num--) {
             System.out.println(num);
         }
+
+        int[] array = new int[] {1, 2, 3, 4, 5};
+        int right = 0;
+        int tmp = 0;
+        System.out.println(array[0] + ", " + array[1] + ", " + array[2] + ", " + array[3] + ", " + array[4]);
+        for (int left = 1; (array.length / 2) >= left; left++) {
+            right = array.length - left;
+            tmp = array[right];
+            array[right] = array[left];
+            array[left] = tmp;
+        }
+        System.out.println(array[0] + ", " + array[1] + ", " + array[2] + ", " + array[3] + ", " + array[4]);
     }
 }
