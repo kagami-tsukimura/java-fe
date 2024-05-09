ef7af2b4c97f2792b6aed4145b369ed1107e4472
Kagami <shimura.kyohei7617@gmail.com>
2024-05-09 20:43:00 +0900

    auto: auto command push

--- src/sec6/Main.java
+++ src/sec6/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        for (int num = 1; num <= 10; num++) {
+        for (int num = 10; num > 0; num--) {
             System.out.println(num);
         }
     }
