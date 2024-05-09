c9fbd59a7e8db6a7523a8e2e78efeec62b0d4a90
Kagami <shimura.kyohei7617@gmail.com>
2024-05-09 22:27:24 +0900

    auto: auto command push

--- src/sec7/Main.java
+++ src/sec7/Main.java
@@ -3,11 +3,13 @@ package sec7;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        function();
+        int i = 0;
+        function(i);
     }
 
-    public static void function() {
+    public static void function(int i) {
         System.out.println("function");
+        System.out.println(i + 100);
     }
     
 }
