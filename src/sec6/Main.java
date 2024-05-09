1ee88263e0a025380cf733f0c926e017f34efa58
Kagami <shimura.kyohei7617@gmail.com>
2024-05-09 20:39:53 +0900

    auto: auto command push

--- src/sec6/Main.java
+++ src/sec6/Main.java
@@ -3,5 +3,9 @@ package sec6;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
