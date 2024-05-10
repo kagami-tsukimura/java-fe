e40ac600fdcc04c905263ba3eec0838af64a5ba2
Kagami <shimura.kyohei7617@gmail.com>
2024-05-10 20:44:50 +0900

    auto: auto command push

---
+++ src/sec7/Branch.java
@@ -0,0 +1,21 @@
+package sec7;
+
+public class Branch {
+    public static void main(String[] args) {
+        int age = 10;
+        System.out.println(fee(age));
+    }
+
+    private static int fee(int age) {
+        int ret;
+        if(age <= 3) {
+            ret = 100;
+        }else if(age <= 9) {
+            ret = 300;
+        }else {
+            ret = 500;
+        }
+        return ret;
+    }
+    
+}
