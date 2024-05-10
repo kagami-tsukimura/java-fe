e1b46de18e529d6e2ee4a5386ff347057fd50c56
Kagami <shimura.kyohei7617@gmail.com>
2024-05-11 07:52:09 +0900

    auto: auto command push

--- src/sec8/Main.java
+++ src/sec8/Main.java
@@ -1,18 +1,54 @@
 package sec8;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        run();
-        Log log = new Log();
-        Log log2 = new Log();
-        log.run();
-        int result = log.calc(64);
-        System.out.println(result);
-
-        log2.run();
     }
 
-    private static void run() {
-        System.out.println("Hello World_Main!");
+    private static void deleteFile(){
+        Path p = Paths.get("shape.html");
+
+        try{
+            Files.deleteIfExists(p);
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+
+    private static void drawHeader(){
+        String source =
+                "<!DOCTYPE html>" +
+                        "<html>" +
+                        "<head>" +
+                        "<meta charset=\"utf-8\">" +
+                        "<title>SVG Test</title>" +
+                        "</head>" +
+                        "<body>" +
+                        "<svg x=0 y=0 width=1000 height=1000 style=\"background-color: #fff\">";
+        writeFile(source);
+    }
+
+    private static void drawFooter() {
+        String source =
+                "</svg>" +
+                        "</body>" +
+                        "</html>";
+        writeFile(source);
+    }
+
+    private static void writeFile(String source) {
+        try{
+            File file = new File("shape.html");
+            FileWriter filewriter = new FileWriter(file, true);
+            filewriter.write(source);
+            filewriter.close();
+        }catch(IOException e){
+            System.out.println(e);
+        }
     }
 }
