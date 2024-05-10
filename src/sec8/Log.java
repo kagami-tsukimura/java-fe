d9ee345677b35767d211bf2f919c06515308b993
Kagami <shimura.kyohei7617@gmail.com>
2024-05-11 00:53:35 +0900

    auto: auto command push

--- src/sec8/Log.java
+++ src/sec8/Log.java
@@ -1,8 +1,7 @@
 package sec8;
 
 public class Log {
-    public static void main(String[] args) {
+    public static void run() {
         System.out.println("Hello World_Log!");
-        Main.main(args);
     }
 }
--- src/sec8/Main.java
+++ src/sec8/Main.java
@@ -3,6 +3,7 @@ package sec8;
 public class Main {
     public static void main(String[] args) {
         run();
+        Log.run();
     }
 
     private static void run() {
