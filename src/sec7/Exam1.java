6edac58a327faadca46fe2089b9c0b5ba72dfd45
Kagami <shimura.kyohei7617@gmail.com>
2024-05-10 21:00:36 +0900

    auto: auto command push

---
+++ src/sec7/Exam1.java
@@ -0,0 +1,18 @@
+package sec7;
+
+public class Exam1 {
+
+    public static void main(String[] args) {
+        int [][] matrix = transformSparseMatrix(null);
+        System.err.println(matrix[0][0]);
+    }
+
+    private static int[][] transformSparseMatrix(int[][] matrix) {
+        int i, j;
+        int [][] sparseMatrix;
+        sparseMatrix = new int[999][99];
+
+        return sparseMatrix;
+        
+    }
+}
