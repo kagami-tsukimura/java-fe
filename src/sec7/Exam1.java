e089416aa9b80ccd1d4d315f4a3e84959f2ff5a3
Kagami <shimura.kyohei7617@gmail.com>
2024-05-10 21:02:09 +0900

    auto: auto command push

--- src/sec7/Exam1.java
+++ src/sec7/Exam1.java
@@ -3,8 +3,15 @@ package sec7;
 public class Exam1 {
 
     public static void main(String[] args) {
-        int [][] matrix = transformSparseMatrix(null);
-        System.err.println(matrix[0][0]);
+        int [][] array = new int[][] {
+            {3, 0, 0, 0, 0},
+            {0, 2, 2, 0, 0},
+            {0, 0, 0, 1, 3},
+            {0, 0, 0, 2, 0},
+            {3, 0, 0, 0, 1},
+        };
+        transformSparseMatrix(array);
+        System.out.println(array);
     }
 
     private static int[][] transformSparseMatrix(int[][] matrix) {
