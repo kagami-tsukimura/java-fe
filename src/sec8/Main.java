99f298997f966f91b1fb3b7cc873a0716409819b
Kagami <shimura.kyohei7617@gmail.com>
2024-05-11 00:58:06 +0900

    auto: auto command push

--- src/sec8/Main.java
+++ src/sec8/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) {
         run();
         Log log = new Log();
+        Log log2 = new Log();
         log.run();
         int result = log.calc(64);
         System.out.println(result);
+
+        log2.run();
     }
 
     private static void run() {
