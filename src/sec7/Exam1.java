fd4e369878cfe2943edc0b4ea9a7826241aa27f7
Kagami <shimura.kyohei7617@gmail.com>
2024-05-10 22:17:18 +0900

    auto: auto command push

--- src/sec7/Exam1.java
+++ src/sec7/Exam1.java
@@ -8,23 +8,26 @@ public class Exam1 {
             {0, 2, 2, 0, 0},
             {0, 0, 0, 1, 3},
             {0, 0, 0, 2, 0},
-            {3, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1},
         };
-        transformSparseMatrix(array);
-        System.out.println(array);
+        int[][] sparseMatrix = transformSparseMatrix(array);
+        System.out.println(sparseMatrix);
     }
 
     private static int[][] transformSparseMatrix(int[][] matrix) {
         int i, j;
         int [][] sparseMatrix;
         sparseMatrix = new int[3][999];
+        int tail = 0;
 
-        for(i = 1; i <= matrix.length; i++) {
-            for(j = 1; j <= matrix[0].length; j++) {
-                // if(matrix[i][j] != 0) {
-                //     sparseMatrix[1] = i;
-                //     sparseMatrix[2] = j;
-                //     sparseMatrix[3] = matrix[i][j];
+        for(i = 0; i <= matrix.length -1; i++) {
+            for(j = 0; j <= matrix[0].length -1; j++) {
+                if(matrix[i][j] != 0) {
+                    sparseMatrix[0][tail] = i;
+                    sparseMatrix[1][tail] = j;
+                    sparseMatrix[2][tail] = matrix[i][j];
+                    tail++;
+                }
             }
         }
 
