762f86e7e0b312effc7447da1a82b76a9f356ed8
Kagami <shimura.kyohei7617@gmail.com>
2024-05-10 23:31:14 +0900

    auto: auto command push

--- src/sec7/Recursive.java
+++ src/sec7/Recursive.java
@@ -5,6 +5,9 @@ public class Recursive {
     public static void main(String[] args) {
         int result = factorial(4);
         System.out.println(result);
+
+        int res = calc(8);
+        System.out.println(res);
     }
 
     private static int factorial(int num) {
@@ -15,6 +18,15 @@ public class Recursive {
             return 1;
         }
         return 0;
+    }
+
+    private static int calc(int num){
+        int result = 1;
+        
+        for(int i = num; i > 1; i--){
+            result *= i;
+        }
+        return result;
     }
 }
 
