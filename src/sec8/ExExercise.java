04a99915f3507761164edb3abcc68d54e4a90eab
Kagami <shimura.kyohei7617@gmail.com>
2024-05-11 09:58:53 +0900

    auto: auto command push

---
+++ src/sec8/ExExercise.java
@@ -0,0 +1,10 @@
+package sec8;
+
+public class ExExercise {
+    public static void main(String[] args) {
+        ExCircle circle = new ExCircle();
+        ExSquare square = new ExSquare();
+        ExTriangle triangle = new ExTriangle();
+        
+    }
+}
